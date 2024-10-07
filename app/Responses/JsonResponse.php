1d37bd0906ef4411769d47d9b64097587bc0f78d
HaslanDaitx <164122728+HaslanDaitx@users.noreply.github.com>
2024-10-06 23:28:53 -0300

    enviando os arquivos

---
+++ app/Responses/JsonResponse.php
@@ -0,0 +1,29 @@
+<?php 
+
+
+
+namespace app\Responses;
+
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
+
+class JsonResponse
+{
+    public static function success(?string $message = null, mixed $data = null)
+    {
+        return response()->json(
+            ['message' => $message, 
+            'data' => $data,
+            'code' => HttpFoundationResponse::HTTP_OK
+        ], HttpFoundationResponse::HTTP_OK);
+    }
+
+    
+    public static function error(string $message, int $httpCode)
+    {
+        return response()->json(
+            ['message' => $message, 
+            'code' => $httpCode
+        ], $httpCode);
+    }
+}
